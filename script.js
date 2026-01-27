b9af610f92eecc312be561a3d44fcc91b5299734
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2026-01-27 15:00:35 -0800

    changed it to modals

--- script.js
+++ script.js
@@ -19,21 +19,31 @@
 //   });
 // });
 
-document.addEventListener("DOMContentLoaded", () => {
-  // Select ALL links you want to participate
-  const links = document.querySelectorAll(".nav__links a, .category a");
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
 
-  const currentPath = window.location.pathname;
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
 
-  links.forEach((link) => {
-    link.addEventListener("click", function (e) {
-      const linkPath = new URL(link.href).pathname;
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
 
-      if (linkPath === currentPath) {
-        link.classList.add("active");
-      } else {
-        link.classList.remove("active");
-      }
-    });
-  });
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener("click", openModal);
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  // console.log(e.key);
+
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
 });
